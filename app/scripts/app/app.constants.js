4b3285f2f602b744df1a2a2657cb01ed9d7c0aff
Maxime Tricoire <max.tricoire@gmail.com>
2015-03-30 10:56:56 +0200

    update ws.braindead.fr to ws.kevoree.org

--- app/scripts/app/app.constants.js
+++ app/scripts/app/app.constants.js
@@ -10,7 +10,7 @@ angular.module('browserApp')
   .constant('APP_NAME',          'BrowserRuntime')
   .constant('APP_ID',             uuid.v4())
   .constant('GROUP_NAME',        'sync')
-  .constant('WS_HOST',           'ws.braindead.fr')
+  .constant('WS_HOST',           'ws.kevoree.org')
   .constant('WS_PORT',           '80')
   .constant('MODULES_PATH',      '_fake_dir_')
   .constant('NPM_REGISTRY_URL',  'http://registry.npmjs.org/{name}/-/{name}-{version}.tgz');
